aa50f147e274f1eaeae9e9762195625333d36100
joshuanatan <joshuanatan98@gmail.com>
2020-03-26 17:46:25 +0700

    Update content.php

--- application/views/dashboard/content.php
+++ application/views/dashboard/content.php
@@ -1,4 +1,4 @@
 <div class = "col-lg-12">
-    <h2>Welcome to Formula Counter Module!</h2>
+    <h2>Welcome to Formula Management Module!</h2>
     <p>This module will help you to manage your formulas and help you to use those formulas</p>
 </div>
