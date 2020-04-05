8362e70c0d9a56f75fe697f78f5700233ee597d7
joshuanatan <joshuanatan98@gmail.com>
2020-04-05 20:51:30 +0700

    alat

---
+++ application/views/alat/page_open.php
@@ -0,0 +1,17 @@
+<div class="page">
+    <div class="page-header">
+        <h1 class="page-title">Daftar Alat</h1>
+        <div class="page-header-actions">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="<?php echo base_url();?>welcome/dashboard">Home</a></li>
+                <li class="breadcrumb-item active">Alat</li>
+            </ol>
+        </div>
+    </div>
+    <div class="page-content">
+    <!-- Panel -->
+        <div class="panel">
+            <div class="panel-body container-fluid">
+                <div class="row row-lg">
+                    <!-- <div class = "col-lg-6"> -->
+                    
