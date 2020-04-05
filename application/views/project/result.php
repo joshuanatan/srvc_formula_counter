aca8fba8ee2c3f6e46193efd69d98982086b4c52
joshuanatan <joshuanatan98@gmail.com>
2020-04-05 20:52:22 +0700

    done perhitungan

--- application/views/project/result.php
+++ application/views/project/result.php
@@ -2,18 +2,22 @@
     <table class = "table table-bordered table-hover table-striped" data-plugin = "dataTable">
         <thead>
             <th>Attribute Name</th>
+            <th>Volume</th>
             <th>Coefficient</th>
-            <th>Units</th>
             <th>Estimated Unit</th>
+            <th></th>
+            <th>Unit Price</th>
             <th>Estimated Price</th>
         </thead>
         <tbody>
             <?php for($a = 0; $a<count($rab); $a++):?>
             <tr>
                 <td><?php echo strtoupper($rab[$a]["formula_attr_name"]);?></td>
-                <td><?php echo $rab[$a]["koefisien"];?></td>
                 <td><?php echo $rab[$a]["satuan_htg"];?></td>
+                <td><?php echo $rab[$a]["koefisien"];?></td>
                 <td><?php echo $rab[$a]["jumlah_unit"];?></td>
+                <td><?php echo $rab[$a]["satuan_attr"];?></td>
+                <td><?php echo $rab[$a]["harga_satuan_attr"];?></td>
                 <td><?php echo $rab[$a]["harga_satuan"];?></td>
             </tr>
             <?php endfor;?>
