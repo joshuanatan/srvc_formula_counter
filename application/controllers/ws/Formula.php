52f150fb8bdce6dbfb01d9cbda930a34e7208b1d
joshuanatan <joshuanatan.jn@gmail.com>
2020-04-27 01:33:19 +0700

    update formula

--- application/controllers/ws/Formula.php
+++ application/controllers/ws/Formula.php
@@ -77,6 +77,226 @@ class Formula extends CI_Controller{
 
         echo json_encode($respond);
     }
+    public function register(){
+        $respond["status"] = "SUCCESS";
+        $config = array(
+            array(
+                "field" => "id_formula_cat",
+                "label" => "Formula Category",
+                "rules" => "required"
+            ),
+            array(
+                "field" => "formula_desc",
+                "label" => "Formula Description",
+                "rules" => "required|max_length[300]"
+            ),
+
+        );
+        $this->form_validation->set_rules($config);
+        if($this->form_validation->run()){
+            $this->load->model("m_formula");
+            $id_formula_cat = $this->input->post("id_formula_cat");
+            $formula_desc = $this->input->post("formula_desc");
+
+            $this->m_formula->set_id_formula_cat($id_formula_cat);
+            $this->m_formula->set_formula_desc($formula_desc);
+            $id_mstr_formula = $this->m_formula->insert();
+
+            if($id_mstr_formula){
+                $check = $this->input->post("check");
+                if($check != ""){
+                    foreach($check as $a){
+                        $config = array(
+                            array(
+                                "field" => "id_attr".$a,
+                                "label" => "Attribute",
+                                "rules" => "required"
+                            ),
+                            array(
+                                "field" => "koefisien".$a,
+                                "label" => "Koefisien",
+                                "rules" => "required"
+                            )
+                        );
+                        $this->form_validation->set_rules($config);
+                        if($this->form_validation->run()){
+                            $this->load->model("m_formula_comb");
+
+                            $id_formula_attr = $this->input->post("id_attr".$a);
+                            $koefisien = $this->input->post("koefisien".$a);
+                            $this->m_formula_comb->set_id_mstr_formula($id_mstr_formula);
+                            $this->m_formula_comb->set_id_formula_attr($id_formula_attr);
+                            $this->m_formula_comb->set_koefisien($koefisien);
+                            $this->m_formula_comb->insert();
+                        }
+                        else{
+                            $respond["status"] = "ERROR";
+                            $respond["msg"] = validation_errors();
+                        }
+                    }
+                }
+            }
+        }
+        else{
+            $respond["status"] = "ERROR";
+            $respond["msg"] = validation_errors();
+        }
+        echo json_encode($respond);
+    }
+    public function update(){
+        $respond["status"] = "SUCCESS";
+        $config = array(
+            array(
+                "field" => "id_formula",
+                "label" => "Formula ID",
+                "rules" => "required"
+            ),
+            array(
+                "field" => "id_formula_cat",
+                "label" => "",
+                "rules" => "required"
+            ),
+            array(
+                "field" => "formula_desc",
+                "label" => "Formula Description",
+                "rules" => "required"
+            ), 
+        );
+        $this->form_validation->set_rules($config);
+        if($this->form_validation->run()){
+            
+            $this->load->model("m_formula");
+            $id_submit_formula = $this->input->post("id_formula");
+            $id_formula_cat = $this->input->post("id_formula_cat");
+            $formula_desc = $this->input->post("formula_desc");
+            
+            $this->m_formula->set_id_submit_formula($id_submit_formula);
+            $this->m_formula->set_id_formula_cat($id_formula_cat);
+            $this->m_formula->set_formula_desc($formula_desc);
+            $this->m_formula->update();
+
+            $edit = $this->input->post("edit");
+            if($edit != ""){
+                foreach($edit as $a){
+                    $config = array(
+                        array(
+                            "field" => "id_formula_comb".$a,
+                            "label" => "ID Formula Combination",
+                            "rules" => "required|integer" 
+                        ),
+                        array(
+                            "field" => "attr_name".$a,
+                            "label" => "Formula Attribute",
+                            "rules" => "required"
+                        ),
+                        array(
+                            "field" => "koefisien".$a,
+                            "label" => "Koefisien",
+                            "rules" => "required"
+                        )
+                    );
+                    $this->form_validation->set_rules($config);
+                    if($this->form_validation->run()){
+                        $this->load->model("m_formula_comb");
+
+                        $id_formula_comb = $this->input->post("id_formula_comb".$a);
+                        $attr_name = $this->input->post("attr_name".$a);
+                        $where = array(
+                            "formula_attr_name" => $attr_name
+                        );
+                        $id_formula_attr = get1Value("mstr_formula_attr","id_submit_formula_attr",$where);
+                        $koefisien = $this->input->post("koefisien".$a);
+                        
+                        $this->m_formula_comb->set_id_mstr_formula($id_submit_formula);
+                        $this->m_formula_comb->set_id_submit_formula_comb($id_formula_comb);
+                        $this->m_formula_comb->set_id_formula_attr($id_formula_attr);
+                        $this->m_formula_comb->set_koefisien($koefisien);
+                        $this->m_formula_comb->update();
+                    }
+                    else{
+                        $respond["status"] = "ERROR";
+                        $respond["msg"] = validation_errors();
+                    }
+                }
+            }
+            
+            $delete = $this->input->post("delete");
+            if($delete != ""){
+                foreach($delete as $a){
+                    $config = array(
+                        array(
+                            "field" => "id_formula_comb".$a,
+                            "label" => "ID Formula Combination",
+                            "rules" => "required|integer" 
+                        )
+                    );
+                    $this->form_validation->set_rules($config);
+                    if($this->form_validation->run()){
+                        $this->load->model("m_formula_comb");
+
+                        $id_formula_comb = $this->input->post("id_formula_comb".$a);
+
+                        $this->m_formula_comb->set_id_submit_formula_comb($id_formula_comb);
+                        $this->m_formula_comb->delete();
+                    }
+                    else{
+                        $respond["status"] = "ERROR";
+                        $respond["msg"] = validation_errors();
+                    }
+                }
+            }
+            $check = $this->input->post("check");
+            if($check != ""){
+                foreach($check as $a){
+                    $config = array(
+                        array(
+                            "field" => "id_attr".$a,
+                            "label" => "Attribute",
+                            "rules" => "required"
+                        ),
+                        array(
+                            "field" => "koefisien".$a,
+                            "label" => "Koefisien",
+                            "rules" => "required"
+                        )
+                    );
+                    $this->form_validation->set_rules($config);
+                    if($this->form_validation->run()){
+                        $this->load->model("m_formula_comb");
+
+                        $id_formula_attr = $this->input->post("id_attr".$a);
+                        $koefisien = $this->input->post("koefisien".$a);
+                        $this->m_formula_comb->set_id_mstr_formula($id_submit_formula);
+                        $this->m_formula_comb->set_id_formula_attr($id_formula_attr);
+                        $this->m_formula_comb->set_koefisien($koefisien);
+                        $this->m_formula_comb->insert();
+                    }
+                    else{
+                        $respond["status"] = "ERROR";
+                        $respond["msg"] = validation_errors();
+                    }
+                }
+            }
+        }
+        else{
+            $respond["status"] = "ERROR";
+            $respond["msg"] = validation_errors();
+        }
+        echo json_encode($respond);
+    }
+    public function delete($id_formula){
+        $respond["status"] = "SUCCESS";
+        if($id_formula != "" && is_numeric($id_formula)){
+            $this->load->model("m_formula");
+            $this->m_formula->set_id_submit_formula($id_formula);
+            $this->m_formula->delete();
+        }
+        else{
+            $respond["status"] = "ERROR";
+            $respond["msg"] = validation_errors();
+        }
+        echo json_encode($respond);
+    }
     public function subformula($id_formula){
         $respond["status"] = "SUCCESS";
         $respond["main"] = array();
@@ -149,6 +369,100 @@ class Formula extends CI_Controller{
         }
         else{
             $respond["status"] = "ERROR";
+        }
+        echo json_encode($respond);
+    }
+    public function list_category(){
+        $respond["status"] = "SUCCESS";
+        $respond["content"] = array();
+
+        $order_by = $this->input->get("orderBy");
+        $order_direction = $this->input->get("orderDirection");
+        $page = $this->input->get("page");
+        $search_key = $this->input->get("searchKey");
+
+        $data_per_page = 10;
+        
+        $this->load->model("m_formula_cat");
+        $result = $this->m_formula_cat->content($page,$order_by,$order_direction,$search_key,$data_per_page);
+
+        if($result["data"]->num_rows() > 0){
+            $result["data"] = $result["data"]->result_array();
+            for($a = 0; $a<count($result["data"]); $a++){
+                $respond["content"][$a]["id"] = $result["data"][$a]["id_submit_formula_cat"];
+                $respond["content"][$a]["name"] = $result["data"][$a]["formula_cat_name"];
+                $respond["content"][$a]["status"] = $result["data"][$a]["status_formula_cat"];
+                $respond["content"][$a]["last_modified"] = $result["data"][$a]["formula_cat_last_modified"];
+            }
+        }
+        else{
+            $respond["status"] = "ERROR";
+        }
+        $respond["page"] = $this->pagination->generate_pagination_rules($page,$result["total_data"],$data_per_page);
+
+        echo json_encode($respond);
+    }
+    public function register_cat(){
+        $respond["status"] = "SUCCESS";
+        $config = array(
+            array(
+                "field" => "cat_name",
+                "label" => "Formula Category Name",
+                "rules" => "required"
+            )
+        );
+        $this->form_validation->set_rules($config);
+        if($this->form_validation->run()){
+            $formula_cat_name = $this->input->post("cat_name");
+            $this->load->model("m_formula_cat");
+            $this->m_formula_cat->set_formula_cat_name($formula_cat_name);
+            $this->m_formula_cat->insert();
+        }
+        else{
+            $respond["status"] = "ERROR";
+            $respond["msg"] = validation_errors();
+        }
+        echo json_encode($respond);
+    }
+    public function update_cat(){
+        $respond["status"] = "SUCCESS";
+        $config = array(
+            array(
+                "field" => "id_cat",
+                "label" => "",
+                "rules" => "required"
+            ),
+            array(
+                "field" => "cat_name",
+                "label" => "Formula Category Name",
+                "rules" => "required"
+            )
+        );
+        $this->form_validation->set_rules($config);
+        if($this->form_validation->run()){
+            $formula_cat_name = $this->input->post("cat_name");
+            $id_submit_formula_cat = $this->input->post("id_cat");
+            $this->load->model("m_formula_cat");
+            $this->m_formula_cat->set_id_submit_formula_cat($id_submit_formula_cat);
+            $this->m_formula_cat->set_formula_cat_name($formula_cat_name);
+            $this->m_formula_cat->update();
+        }
+        else{
+            $respond["status"] = "ERROR";
+            $respond["msg"] = validation_errors();
+        }
+        echo json_encode($respond);
+    }
+    public function delete_cat($id_cat){
+        $respond["status"] = "SUCCESS";
+        if($id_cat != "" && is_numeric($id_cat)){
+            $this->load->model("m_formula_cat");
+            $this->m_formula_cat->set_id_submit_formula_cat($id_cat);
+            $this->m_formula_cat->delete();
+        }
+        else{
+            $respond["status"] = "ERROR";
+            $respond["msg"] = validation_errors();
         }
         echo json_encode($respond);
     }
--- application/models/M_formula_cat.php
+++ application/models/M_formula_cat.php
@@ -8,11 +8,63 @@ class M_formula_cat extends CI_Model{
     private $formula_cat_name = "";
     private $formula_cat_last_modified = "";
     private $id_last_modified = 0;
+    private $column_list = array();
 
     public function __construct(){
         parent::__construct();
         $this->formula_cat_last_modified = date("Y-m-d H:i:s");
         $this->id_last_modified = $this->session->id_submit_acc;
+        $this->column_list = array(
+            array(
+                "col_name" => "formula_cat_name",
+                "col_disp" => "Kategori Pekerjaan",
+                "order_by" => true
+            ),
+            array(
+                "col_name" => "status_formula_cat",
+                "col_disp" => "Status Kategori",
+                "order_by" => false
+            ),
+            array(
+                "col_name" => "formula_cat_last_modified",
+                "col_disp" => "Last Modified",
+                "order_by" => false
+            ),
+        );
+    }
+    public function column(){
+        return $this->column_list;
+    }
+    public function content($page = 1,$order_by = 0, $order_direction = "ASC", $search_key = "",$data_per_page = ""){
+        $order_by = $this->column_list[$order_by]["col_name"];
+        $search_query = "";
+        if($search_key != ""){
+            $search_query .= "AND
+            ( 
+                id_submit_formula_cat LIKE '%".$search_key."%' OR
+                formula_cat_name LIKE '%".$search_key."%' OR
+                status_formula_cat LIKE '%".$search_key."%' OR
+                formula_cat_last_modified LIKE '%".$search_key."%'
+            )";
+        }
+        $query = "
+        SELECT id_submit_formula_cat,formula_cat_name,status_formula_cat,formula_cat_last_modified
+        FROM ".$this->tbl_name." 
+        WHERE status_formula_cat = ? ".$search_query."  
+        ORDER BY ".$order_by." ".$order_direction." 
+        LIMIT 10 OFFSET ".($page-1)*$data_per_page;
+        $args = array(
+            "ACTIVE"
+        );
+        $result["data"] = executeQuery($query,$args);
+        
+        $query = "
+        SELECT id_submit_formula_cat
+        FROM ".$this->tbl_name." 
+        WHERE status_formula_cat = ? ".$search_query."  
+        ORDER BY ".$order_by." ".$order_direction;
+        $result["total_data"] = executeQuery($query,$args)->num_rows();
+        return $result;
     }
     public function list(){
         $where = array(
